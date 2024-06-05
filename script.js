0789b0e6c9158222c4eea87745a46df0a06a7fa4
EduardaBorgesD <medafbo@gmail.com>
2024-06-05 14:12:36 +0100

    O que já tinha
    Código Inicial

---
+++ script.js
@@ -0,0 +1,46 @@
+function mudarparaAlberto() {
+    // Esconder o conteúdo atual
+    document.getElementById('fernandopessoa').classList.toggle('hidden');
+    document.getElementById('albertocaeiro').classList.toggle('hidden');
+}
+
+function mudarparaAlvaro() {
+    // Esconder o conteúdo atual
+    document.getElementById('albertocaeiro').classList.toggle('hidden');
+    document.getElementById('alvarodecampos').classList.toggle('hidden');
+}
+
+function mudarparaRicardo() {
+    // Esconder o conteúdo atual
+    document.getElementById('alvarodecampos').classList.toggle('hidden');
+    document.getElementById('ricardoreis').classList.toggle('hidden');
+}
+
+function mudarparaFernando() {
+    // Esconder o conteúdo atual
+    document.getElementById('ricardoreis').classList.toggle('hidden');
+    document.getElementById('fernandopessoa').classList.toggle('hidden');
+}
+
+function mudarparaRicardo1() {
+    // Esconder o conteúdo atual
+    document.getElementById('fernandopessoa').classList.toggle('hidden');
+    document.getElementById('ricardoreis').classList.toggle('hidden');
+}
+function mudarparaFernando1() {
+    // Esconder o conteúdo atual
+    document.getElementById('albertocaeiro').classList.toggle('hidden');
+    document.getElementById('fernandopessoa').classList.toggle('hidden');
+}
+
+function mudarparaAlvaro1() {
+    // Esconder o conteúdo atual
+    document.getElementById('ricardoreis').classList.toggle('hidden');
+    document.getElementById('alvarodecampos').classList.toggle('hidden');
+}
+
+function mudarparaAlberto1() {
+    // Esconder o conteúdo atual
+    document.getElementById('alvarodecampos').classList.toggle('hidden');
+    document.getElementById('albertocaeiro').classList.toggle('hidden');
+}
