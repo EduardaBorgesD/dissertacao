87bd8796869b7dc3b92f65ef282765f8cd99a31c
EduardaBorgesD <medafbo@gmail.com>
2024-06-16 19:32:26 +0100

    Tentar Fazer Animação SVG
    ainda não funciona bem

--- script3.js
+++ script3.js
@@ -2,8 +2,10 @@ document.addEventListener('keydown', function(event) {
     if (event.code === 'Space') {
         document.getElementById('alvaroinicio').style.display = 'none';
         document.getElementById('alvaromeio').style.display = 'block';
+        Restart();
     }
-});
+    }
+);
 
 document.addEventListener('keyup', function(event) {
     if (event.code === 'Space') {
@@ -11,3 +13,10 @@ document.addEventListener('keyup', function(event) {
         document.getElementById('alvaromeio').style.display = 'none';
     }
 });
+
+function Restart(){
+    var svg = document.getElementById('alvaromeio');
+    var newSVG = svg.cloneNode(true);
+    svg.parentNode.replaceChild(newSVG, svg);
+    
+}
